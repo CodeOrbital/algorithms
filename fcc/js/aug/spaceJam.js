4e3fd5467061a3234c6d0f84a23a4b96dab99217
AmanuCodes <zescholarboy@gmail.com>
2026-09-21 20:28:55 +0300

    add spaceJam.js with concise and also robust codes

---
+++ fcc/js/aug/spaceJam.js
@@ -0,0 +1,36 @@
+function spaceJam(s) {
+  s = s.toUpperCase();
+  let l = s.length;
+  let sArray = [];
+  let c = 0;
+  for (let d = 0; d < l; d++) {
+    if (!/\s/.test(s[d])) {
+      sArray[c] = s[d];
+      c++;
+    }
+  }
+  let jamArray = [];
+  let chunk;
+  for (let n = 0; n < sArray.length; n++) {
+    if (n + 1 != sArray.length) {
+      chunk = sArray[n] + "  ";
+    } else {
+      chunk = sArray[n];
+    }
+    jamArray[n] = chunk;
+  }
+  let jam = "";
+  for (let i = 0; i < jamArray.length; i++) {
+    jam = jam + jamArray[i];
+  }
+  return jam;
+}
+/* Shorter using built-in methods
+function spaceJam(s) {
+  s = s.toUpperCase();
+  let sArray = Array.from(s);
+  sArray = sArray.filter((char) => !/\s/.test(char));
+  let jam = sArray.join("  ");
+  return jam;
+}
+*/
